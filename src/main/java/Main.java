74474ab8d7bfa76ccfe3d9d2c150672b91822c8c
tushar-zeda <tushar.juneja@zeda.io>
2025-05-11 13:59:43 +0530

    Respond to multiple PINGs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,62 @@
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args){
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
-        ServerSocket serverSocket;
+        //  Uncomment this block to pass the first stage
+        ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
-          serverSocket.setReuseAddress(true);
-          while(true) {
-              clientSocket = serverSocket.accept();
-              byte[] input = clientSocket.getInputStream().readAllBytes();
-              String inputString = new String(input);
-              System.out.println("Received \n" + inputString);
-              clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-          }
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            while(true) {
+               
+                try{
+                    clientSocket = serverSocket.accept();
+                    handleClients(clientSocket);
+                }finally {
+                    clientSocket.close();
+                }
             }
-          } catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (serverSocket != null && !serverSocket.isClosed()) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
+
+    private static void handleClients(Socket clientSocket){
+        try {
+            InputStream in = clientSocket.getInputStream();
+            OutputStream out = clientSocket.getOutputStream();
+            byte[] buffer  = new byte[1024];
+            int bytesRead ;
+            
+            while((bytesRead = in.read(buffer)) != -1){
+                String input = new String(buffer,0, bytesRead);
+                if(input.equalsIgnoreCase("PING")){
+                    out.write("+PONG\r\n".getBytes());
+                    out.flush();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error : "+ e.getMessage());
+        }
+
+    }
 }
