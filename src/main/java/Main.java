615880556540cb52e286fbf1c589711b1d3d79d8
tushar-zeda <tushar.juneja@zeda.io>
2025-05-11 14:17:52 +0530

    Handling concurrent clients

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,18 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args){
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
-
+        ExecutorService executor = Executors.newFixedThreadPool(10);
         //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
+
         int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
@@ -19,20 +22,27 @@ public class Main {
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             while(true) {
-               
-                try{
-                    clientSocket = serverSocket.accept();
-                    handleClients(clientSocket);
-                }finally {
-                    clientSocket.close();
-                }
+                final Socket clientSocket = serverSocket.accept();
+
+                executor.submit(() ->{
+                    try{
+                        handleClients(clientSocket);
+                    }finally {
+                        try {
+                            clientSocket.close();
+                        } catch (IOException e) {
+                            System.out.println("IOException \n"+ e.getMessage());
+                        }
+                    }
+                });
+
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
             try {
                 if (serverSocket != null && !serverSocket.isClosed()) {
-                    clientSocket.close();
+                    serverSocket.close();
                 }
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
@@ -48,6 +58,7 @@ public class Main {
             int bytesRead ;
             
             while((bytesRead = in.read(buffer)) != -1){
+
                 String input = new String(buffer,0, bytesRead);
                 out.write("+PONG\r\n".getBytes());
                 out.flush();
